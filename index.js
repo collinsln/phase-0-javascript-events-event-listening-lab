3a374725790f8dfa3066b3852cc649e6e0916ded
collinsln <collangat@gmail.com>
2023-09-30 12:47:14 +0300

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const input=document.getElementById('input');
 
 function addingEventListener() {
-    function clickAlert() {
-        alert('I was clicked!');
-      }
+    
       
-      input.addEventListener('click', clickAlert);
+      input.addEventListener('click', () => alert (`i was clicked`));
     
 }
 addingEventListener();
